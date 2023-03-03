775e2c7f9e9097eee41d2463e3cd78d343a7f462
ChSt15 <chrissteffen98@gmail.com>
2023-03-03 09:09:32 +0100

    Changed to force matrix and vector type.

--- include/ExVectrData/value_covariance.hpp
+++ include/ExVectrData/value_covariance.hpp
@@ -1,6 +1,8 @@
 #ifndef EXVECTRDATA_VALUECOVARIANCE_H
 #define EXVECTRDATA_VALUECOVARIANCE_H
 
+#include "ExVectrMath/matrix_base.hpp"
+#include "ExVectrMath/matrix_vector.hpp"
 
 namespace VCTR
 {
@@ -11,17 +13,18 @@ namespace VCTR
         /**
          * Simple container class for storing values and their covariances.
          * @note Easy constructor by -> ValueCov{value, covariance}
+         * @tparam TYPE Value type. Float, double, int etc.
+         * @tparam NUMVALUES Number of values. Basically length of value vector and width height of covariance matrix.
          */
-        template <typename VALUETYPE, typename COVARIANCETYPE>
+        template <typename TYPE, size_t NUMVALUES>
         struct ValueCov
         {
         public:
             /// @brief Values of sensor data.
-            VALUETYPE val = 0;
+            Math::Vector<TYPE, NUMVALUES> val = 0;
 
             /// @brief Covariance of sensor values.
-            COVARIANCETYPE cov = 0;
-
+            Math::Matrix<TYPE, NUMVALUES, NUMVALUES> cov = 0;
         };
 
     }
