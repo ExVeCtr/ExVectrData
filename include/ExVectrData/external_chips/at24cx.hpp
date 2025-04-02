030b453683eb1df0319179c92937861b5ab1064c
ChSt15 <chrissteffen98@gmail.com>
2025-04-02 16:23:37 +0200

    Added driver for the at24cx

---
+++ include/ExVectrData/external_chips/at24cx.hpp
@@ -0,0 +1,114 @@
+#ifndef EXVECTRDATA_MEMORYCACHE_HPP_
+#define EXVECTRDATA_MEMORYCACHE_HPP_
+
+#include "stdint.h"
+#include "stddef.h"
+
+#include "ExVectrHAL/digital_io.hpp"
+
+#include "ExVectrData/memory_interface.hpp"
+
+
+namespace VCTR
+{
+
+    namespace Data
+    {
+
+        /**
+         * @brief This class implements the Memory_Interface for the AT24CX series of EEPROMs.
+         */
+        class Memory_AT24CX : public Memory_Interface
+        {
+        private:
+            
+            static constexpr uint8_t AT24CX_ID = 0x50; //AT24CX ID
+
+            bool initialised_ = false; //If the memory has been initialised and is in working condition
+
+            HAL::DigitalIO *ioBus_ = nullptr;
+
+            size_t memorySize_ = 0; //Size of the memory in bytes
+            size_t pageSize_ = 0; //Size of the memory page in bytes
+            
+
+        public:
+
+            /**
+             * @brief Constructor.
+             * @param memorySize Size of the memory in bytes.
+             * @param pageSize Size of the memory page in bytes. Normally 32 bytes.
+             */
+            Memory_AT24CX(size_t memorySize, size_t pageSize = 32);
+
+            /**
+             * @brief Initialises the memory.
+             * @param ioBus IO bus to use for communications.
+             */
+            bool initMemory(HAL::DigitalIO &ioBus);
+            
+            /**
+             * @brief Reads data from the memory.
+             * @param bufferPtr Pointer to the buffer where the data will be written to.
+             * @param numBytes Number of bytes to be read.
+             * @param index Index from where the data will be read from the memory
+             * @param bufferIndex Index to where the data will be written in the buffer.
+             * @return Number of bytes read from the memory and placed into the buffer.
+             */
+            size_t readMem(uint8_t* bufferPtr, size_t numBytes, size_t index, size_t bufferIndex = 0) override;
+            
+            /**
+             * @brief Writes data to the memory.
+             * @param bufferPtr Pointer to the buffer where the data will be read from.
+             * @param numBytes Number of bytes to be written.
+             * @param index Index to where the data will be written to the memory.
+             * @param bufferIndex Index from where the data will be read in the buffer.
+             * @return Number of bytes written to the memory from the buffer.
+             */
+            size_t writeMem(uint8_t const* bufferPtr, size_t numBytes, size_t index, size_t bufferIndex = 0) override;
+
+            /**
+             * @brief Transfers data from given memory to this memory.
+             * @param memToReceiveData Memory to receive data from.
+             * @param numBytes Number of bytes to be transferred.
+             * @param toIndex Index to where the data will be written to this memory.
+             * @param fromIndex Index from where the data will be read in the given memory.
+             * @return Number of bytes written to this memory from the given memory.
+             */
+            size_t transferFrom(Memory_Interface& memToReceiveData, size_t numBytes, size_t toIndex = 0, size_t fromIndex = 0) override;
+
+            /**
+             * @returns the length of the memory in bytes.
+             */
+            size_t size() override;
+
+        private:
+
+            /**
+             * @brief Writes data to the device on the bus.
+             */
+            bool writeBus(const uint8_t* bufferPtr, size_t numBytes, size_t address);
+
+            /**
+             * @brief Reads data from the device on the bus.
+             */
+            bool readBus(uint8_t* bufferPtr, size_t numBytes, size_t address);
+
+
+        };
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ src/at24cx.cpp
@@ -0,0 +1,142 @@
+#include "stdint.h"
+#include "stddef.h"
+
+#include "ExVectrCore/print.hpp"
+
+#include "ExVectrHAL/digital_io.hpp"
+
+#include "ExVectrData/memory_interface.hpp"
+
+#include "ExVectrData/external_chips/at24cx.hpp"
+
+
+namespace VCTR
+{
+
+    namespace Data
+    {
+
+        
+
+        Memory_AT24CX::Memory_AT24CX(size_t memorySize, size_t pageSize) {
+
+            memorySize_ = memorySize;
+            pageSize_ = pageSize;
+
+        }
+
+
+        bool Memory_AT24CX::initMemory(HAL::DigitalIO &ioBus) {
+
+            if (ioBus.getInputType() != HAL::IO_TYPE_t::BUS_I2C)
+            {
+                VCTR::Core::printE("QMC5883 given incorrect input type. Must be I2C. Given type: %d.\n", ioBus.getInputType());
+                return false;
+            }
+
+            if (ioBus.getOutputType() != HAL::IO_TYPE_t::BUS_I2C)
+            {
+                VCTR::Core::printE("QMC5883 given incorrect output type. Must be I2C. Given type: %d.\n", ioBus.getOutputType());
+                return false;
+            }
+
+            ioBus_ = &ioBus;
+
+        }
+            
+            
+        size_t Memory_AT24CX::readMem(uint8_t* bufferPtr, size_t numBytes, size_t index, size_t bufferIndex = 0) {
+            if (index + numBytes > memorySize_) {
+                VCTR::Core::printE("AT24CX readMem(): index out of bounds!\n");
+                return 0;
+            }
+            if (readBus(bufferPtr, numBytes, index)) {
+                VCTR::Core::printE("AT24CX readMem(): failed to read data from bus!\n");
+                return 0;
+            }
+            return numBytes;
+        }
+        
+        
+        size_t Memory_AT24CX::writeMem(uint8_t const* bufferPtr, size_t numBytes, size_t index, size_t bufferIndex = 0) {
+            if (index + numBytes > memorySize_) {
+                VCTR::Core::printE("AT24CX writeMem(): index out of bounds!\n");
+                return 0;
+            }
+            if (writeBus(bufferPtr, numBytes, index)) {
+                VCTR::Core::printE("AT24CX writeMem(): failed to write data to bus!\n");
+                return 0;
+            }
+            return numBytes;
+        }
+
+        
+        size_t Memory_AT24CX::transferFrom(Memory_Interface& memory, size_t numBytes, size_t toIndex = 0, size_t fromIndex = 0) {
+            if (toIndex + numBytes > memorySize_) {
+                VCTR::Core::printE("AT24CX transferFrom(): index out of bounds!\n");
+                return 0;
+            }
+            uint8_t buffer_[numBytes];
+            if (memory.readMem(buffer_, numBytes, fromIndex) != numBytes) {
+                VCTR::Core::printE("AT24CX transferFrom(): failed to read data from given memory!\n");
+                return 0;
+            }
+            size_t bytesWritten = numBytes;
+            while (bytesWritten < numBytes) {
+
+                auto bytesToWrite = numBytes > pageSize_ ? pageSize_ : numBytes;
+                if (writeBus(buffer_ + bytesWritten, bytesToWrite, toIndex + bytesWritten)) {
+                    VCTR::Core::printE("AT24CX transferFrom(): failed to write data to bus!\n");
+                    return 0;
+                }
+
+                bytesWritten += bytesToWrite;
+
+            }
+            return numBytes;
+        }
+
+        
+        size_t Memory_AT24CX::size() {
+            return memorySize_;
+        }
+
+
+        /**
+         * @brief Writes data to the device on the bus.
+         */
+        bool Memory_AT24CX::writeBus(const uint8_t* bufferPtr, size_t numBytes, size_t address) {
+            if (ioBus_->writeData(&address, 2, false) != 2) {
+                VCTR::Core::printE("AT24CX writeBus(): failed to write address to bus!\n");
+                ioBus_->writeData(&address, 0, true);
+                return false;
+            }
+            if (ioBus_->writeData(bufferPtr, numBytes, true) != numBytes) {
+                VCTR::Core::printE("AT24CX writeBus(): failed to write data to bus!\n");
+                return false;
+            }
+            return true;
+        }
+
+        /**
+         * @brief Reads data from the device on the bus.
+         */
+        bool Memory_AT24CX::readBus(uint8_t* bufferPtr, size_t numBytes, size_t address) {
+            if (ioBus_->writeData(&address, 2, false) != 2) {
+                VCTR::Core::printE("AT24CX writeBus(): failed to write address to bus!\n");
+                ioBus_->writeData(&address, 0, true);
+                return false;
+            }
+            if (ioBus_->readData(bufferPtr, numBytes, true) != numBytes) {
+                VCTR::Core::printE("AT24CX writeBus(): failed to read data from bus!\n");
+                return false;
+            }
+            return true;
+        }
+
+        
+
+    }
+
+}
+
